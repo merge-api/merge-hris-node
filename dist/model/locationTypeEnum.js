bc7c549a158ff992ba2ff22b10f4d86b07b2dc0a
leewang0 <lee@merge.dev>
2022-02-10 11:15:13 -0500

    dist folder wackiness

---
+++ dist/model/locationTypeEnum.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LocationTypeEnum = void 0;
+var LocationTypeEnum;
+(function (LocationTypeEnum) {
+    LocationTypeEnum[LocationTypeEnum["Home"] = 'HOME'] = "Home";
+    LocationTypeEnum[LocationTypeEnum["Work"] = 'WORK'] = "Work";
+})(LocationTypeEnum = exports.LocationTypeEnum || (exports.LocationTypeEnum = {}));
+//# sourceMappingURL=locationTypeEnum.js.map
